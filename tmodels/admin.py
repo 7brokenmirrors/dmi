5e6e71fd078894cad6357ca3ad669d61b307d7b7
Marcel <marcel.jungmann@mailbox.org>
2021-03-10 15:09:18 +0100

    Add CheckConstraints

--- tmodels/admin.py
+++ tmodels/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from tmodels.models import Canine, Feline
 # Register your models here.
+admin.site.register(Canine)
+admin.site.register(Feline)
--- tmodels/models.py
+++ tmodels/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.db.models import F, Q
+from django.db.models.functions import Length
 from typedmodels.models import TypedModel
 
 # Example from https://github.com/craigds/django-typed-models
 
+models.CharField.register_lookup(Length)
 
 class TypedAnimal(TypedModel):
     """
@@ -16,7 +19,13 @@ class TypedAnimal(TypedModel):
     def __repr__(self):
         return u'<%s: %s>' % (self.__class__.__name__, self.name)
 
-
+    class Meta:
+        constraints = [models.CheckConstraint(check=Q(name__length__gt=0), name='Animal must have a name'),
+                       models.CheckConstraint(check=~Q(Q(type__exact='tmodels.canine') & Q(breed__isnull=True)), name='Canines must have a breed'),
+                       models.CheckConstraint(check=~Q(Q(type__exact='tmodels.feline') & Q(breed__isnull=False)), name='Cats must not have a breed'),
+                       models.CheckConstraint(check=~Q(Q(type__exact='tmodels.canine') & Q(mice_eaten__gt=0)), name='Canines must not eat mice'),
+           ]
+        
 class Canine(TypedAnimal):
     breed = models.CharField(
         max_length=200,
